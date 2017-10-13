46d799942716d60fe3a975da1e08cd1a269fa3e7
Paul Correll <pcorrell@users.noreply.github.com>
2017-10-13 08:39:13 -0400

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -41,7 +41,8 @@ $(function() {
           //clear all fields
           $('#contactForm').trigger("reset");
         },
-        error: function() {
+        error: function(data) {
+          console.log('data', data);
           // Fail message
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
